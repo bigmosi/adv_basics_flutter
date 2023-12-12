2c59d0f550c44d00ca52423ebac8f81f384f695a
bigmosi <kinyeramo@gmail.com>
2023-12-12 18:23:48 +0300

    adding questions screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168)
-              ],
-              begin: Alignment.centerLeft,
-              end: Alignment.bottomRight,
-            )),
-            child: const StartScreen())),
-  ));
+  runApp(
+  );
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
 
 class Quiz extends StatefulWidget {
-
+  const Quiz({super.key});
+  
+  @override
+  State createState() {
+    return  _QuizState();
+}
 }
 
-class _QuizState
+
+class _QuizState extends State<Quiz> {
+   @override
+  Widget build(context) {
+   return MaterialApp(
+    home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168)
+              ],
+              begin: Alignment.centerLeft,
+              end: Alignment.bottomRight,
+            )),
+            child: const StartScreen())),
+  );
+  }
+}
