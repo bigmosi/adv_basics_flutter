3090f610c7e1da78b5cfc316223b6aa3c2062d49
bigmosi <kinyeramo@gmail.com>
2023-12-13 13:08:43 +0300

    using tanary operation

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,43 +4,39 @@ import 'package:adv_basics/questons_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
-  
+
   @override
   State createState() {
-    return  _QuizState();
+    return _QuizState();
+  }
 }
-}
-
 
 class _QuizState extends State<Quiz> {
-    Widget? activeScreen;
+  var activeScreen = 'start-screen';
 
-    @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
   }
 
-    void switchScreen() {
-      setState(() {
-        activeScreen = const QuestionsScreen();
-      });
-    }
-
-   @override
+  @override
   Widget build(context) {
-   return MaterialApp(
-    home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168)
-              ],
-              begin: Alignment.centerLeft,
-              end: Alignment.bottomRight,
-            )),
-            child: activeScreen)),
-  );
+    return MaterialApp(
+      home: Scaffold(
+          body: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168)
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.bottomRight,
+              )),
+              child: activeScreen == 'start-screen'
+                  ? StartScreen(switchScreen)
+                  : const QuestionsScreen())),
+    );
   }
 }
